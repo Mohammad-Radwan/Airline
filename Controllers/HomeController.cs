b1c85ca338455f8a060e353a5a2f109537ffe18d
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2024-12-07 00:04:06 +0200

    Adding register redirection (بعد ايه )

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult register()
+    {
+        return View();
+    }
 
     public IActionResult Privacy()
     {
