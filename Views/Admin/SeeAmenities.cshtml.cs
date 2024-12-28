9b2189a52845ed31a9ec0c4ef56c9624937901b8
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2024-12-28 16:20:31 +0200

    Slight edits in lastly done

--- Views/Admin/SeeAmenities.cshtml.cs
+++ Views/Admin/SeeAmenities.cshtml.cs
@@ -2,22 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Airline.Views.Admin;
 using Airline.Models;
-// using System.Data.SqlClient;
-using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+// using Microsoft.Data.SqlClient;
 
 public class SeeAmenitiesModel : PageModel
 {
-    public List<Supplier> Sliers {get; set;} 
+    private readonly string conString = "Server=localhost,1433;Database=AIRLINE;User Id=SA;Password=Mohamed365;TrustServerCertificate=True;";
+    public List<Supplier> Sliers {get; set;} = new List<Supplier>();
+    
     public void OnGet()
     {
-        Sliers = new List<Supplier>();
-        SqlConnection con = sql_helper.GetConnectionObject();
+        // Sliers = new List<Supplier>();
+        SqlConnection con= new SqlConnection(conString);
+        // SqlConnection con = sql_helper.GetConnectionObject();
         string Query = "select * from supplier";
-        List<object> res = sql_helper.MakeCommandWithReturn(Query, con, Mode:"Other");
-        for(int i = 0; i< res.Count; i++){
-            Sliers.Add(new Supplier{
-                ID = res[0].ToString(),
-                Content = res[1].ToString()
+        // List<object> res = sql_helper.MakeCommandWithReturn(Query, con, Mode:"Other");
+        SqlCommand cmd = new SqlCommand(Query, con);
+        SqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine("reading database...");
+            Sliers.Add(new Supplier {
+                ID = reader[0].ToString(),
+                Content = reader[1].ToString()
             });
         }
     }
