8ce5ac90315d78ccf3e8f3f539806dd879b7e01d
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2024-12-30 01:34:34 +0200

    Done ScheduleFlights with two OnPost() handelation remaining the new flight insertion (schema conflict all atributes are not null)

---
+++ WebApplication1/WebApplication1/Pages/FlightA.cs
@@ -0,0 +1,10 @@
+namespace Airline.WebApplication1.WebApplication1.Pages;
+public class FlightA
+{
+    public string fid { get; set; }
+    public string AircraftModel { get; set; }
+    public string DepartureTime { get; set; }
+    public string ArrivalTime { get; set; }
+    public int Duration { get; set; }
+    public string Route { get; set; }
+}
