6eb20fe6df4f046c981d30f37675387c25ae8e1e
Mohamed Mourad <s-mohamed.mmagdy@zewailcity.edu.eg>
2025-01-02 18:02:59 +0200

    update connection string

--- WebApplication1/WebApplication1/Pages/TrackCargo.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/TrackCargo.cshtml.cs
@@ -9,7 +9,7 @@ using System.Data;
 
 public class TrackCargoModel : PageModel
 {
-    private readonly string conString = "Server=localhost,1433;Database=AIRLINE;User Id=SA;Password=Mohamed365;TrustServerCertificate=True;";
+    private readonly string conString = "Server=YOUSSEF_MOHAMAD\\MYMSSQLSERVER;Database=airline_3;Trusted_Connection=True;TrustServerCertificate=True;";
     public List<Cargo> cargos {get; set;} = new List<Cargo>();
     public double sum=0;
     public int count = 0;
